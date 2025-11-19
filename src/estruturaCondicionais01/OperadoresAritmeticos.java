af573c621389934652c0f26ee89c8799b9e4a998
Gabriela <gabrielapaixo@gmail.com>
2025-11-19 19:20:32 -0300

    Primeiro commit

---
+++ src/estruturaCondicionais01/OperadoresAritmeticos.java
@@ -0,0 +1,43 @@
+package estruturaCondicionais01;
+
+public class OperadoresAritmeticos {
+
+	public static void main(String[] args) {
+		System.out.println("--------------------------------------------");
+		System.out.println("             OPERADORES ARITMÉTICOS         ");
+		System.out.println("--------------------------------------------");
+		System.out.println();
+		System.out.println();
+		
+		System.out.println("+ = ADIÇÃO");
+		System.out.println("- = SUBTRAÇÃO");
+		System.out.println("* = MULTIPLICAÇÃO");
+		System.out.println("/ = DIVISÃO");
+		System.out.println("% = RESTO DA DIVISÃO \"MOD\"");
+		System.out.println();
+		System.out.println();
+		
+		System.out.println("PRECEDÊNCIA = PRIMEIRO LUGAR: ()");
+		System.out.println("            = SEGUNDO LUGAR:  * / %");
+		System.out.println("            = TERCEIRO LUGAR: + -");
+		System.out.println();
+		System.out.println();
+
+		System.out.println("EXEMPLOS DE EXPRESSÕES ARITMÉTICAS:");
+		System.out.println("2 * 6 / 3   =  RESULTADO: 4");
+		System.out.println("3 + 2 * 4   =  RESULTADO: 11");
+		System.out.println("(3 + 2) * 4   =  RESULTADO: 20");
+		System.out.println("60 / (3 + 2) * 4   =  RESULTADO: 48");
+		System.out.println("60 / ((3 + 2) * 4)   =  RESULTADO: 3");
+		System.out.println();
+		System.out.println();
+		
+		System.out.println("EXEMPLOS COM O OPERADOR \"MOD\"");
+		System.out.println("14 % 3   =  RESULTADO: 2");
+		System.out.println("19 % 5   =  RESULTADO: 4");
+		System.err.println();
+		System.out.println("EXPLICAÇÃO: POIS QUANDO É REALIZADA A DIVISÃO, O RESULTADO É O RESTO DA DIVISÃO");
+		
+	}
+
+}
